83a1ad42a23d06258390877987fb8aadf48ec2da
jovaniliman <161930917+jovaniliman@users.noreply.github.com>
2024-05-21 10:43:17 +0700

    add_character_chicken

--- MC2/GameViewController.swift
+++ MC2/GameViewController.swift
@@ -10,116 +10,132 @@ import QuartzCore
 import SceneKit
 import GameController
 
-class GameViewController: UIViewController {
-    var virtualController:GCVirtualController?
-    
+class GameViewController: UIViewController, SCNSceneRendererDelegate {
+    var virtualController: GCVirtualController?
+    var ship: SCNNode!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         setupController()
-        // create a new scene
+
+        // Create a new scene
         let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // create and add a camera to the scene
+
+        // Create and add a camera to the scene
         let cameraNode = SCNNode()
         cameraNode.camera = SCNCamera()
         scene.rootNode.addChildNode(cameraNode)
-        
-        // place the camera
+
+        // Place the camera
         cameraNode.position = SCNVector3(x: 0, y: 0, z: 15)
-        
-        // create and add a light to the scene
+
+        // Create and add a light to the scene
         let lightNode = SCNNode()
         lightNode.light = SCNLight()
         lightNode.light!.type = .omni
         lightNode.position = SCNVector3(x: 0, y: 10, z: 10)
         scene.rootNode.addChildNode(lightNode)
-        
-        // create and add an ambient light to the scene
+
+        // Create and add an ambient light to the scene
         let ambientLightNode = SCNNode()
         ambientLightNode.light = SCNLight()
         ambientLightNode.light!.type = .ambient
         ambientLightNode.light!.color = UIColor.darkGray
         scene.rootNode.addChildNode(ambientLightNode)
-        
-        // retrieve the ship node
-        let ship = scene.rootNode.childNode(withName: "ship", recursively: true)!
-        
-        // animate the 3d object
-        ship.runAction(SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: 2, z: 0, duration: 1)))
-        
-        // retrieve the SCNView
+
+        // Retrieve the ship node
+        ship = scene.rootNode.childNode(withName: "ship", recursively: true)
+
+        // Retrieve the SCNView
         let scnView = self.view as! SCNView
-        
-        // set the scene to the view
+
+        // Set the scene to the view
         scnView.scene = scene
-        
-        // allows the user to manipulate the camera
+
+        // Set the view's delegate
+        scnView.delegate = self
+
+        // Allows the user to manipulate the camera
         scnView.allowsCameraControl = true
-        
-        // show statistics such as fps and timing information
+
+        // Show statistics such as fps and timing information
         scnView.showsStatistics = true
-        
-        // configure the view
+
+        // Configure the view
         scnView.backgroundColor = UIColor.black
-        
-        // add a tap gesture recognizer
+
+        // Add a tap gesture recognizer
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
         scnView.addGestureRecognizer(tapGesture)
     }
-    
+
     @objc
     func handleTap(_ gestureRecognize: UIGestureRecognizer) {
-        // retrieve the SCNView
+        // Retrieve the SCNView
         let scnView = self.view as! SCNView
-        
-        // check what nodes are tapped
+
+        // Check what nodes are tapped
         let p = gestureRecognize.location(in: scnView)
         let hitResults = scnView.hitTest(p, options: [:])
-        // check that we clicked on at least one object
+        // Check that we clicked on at least one object
         if hitResults.count > 0 {
-            // retrieved the first clicked object
+            // Retrieved the first clicked object
             let result = hitResults[0]
-            
-            // get its material
+
+            // Get its material
             let material = result.node.geometry!.firstMaterial!
-            
-            // highlight it
+
+            // Highlight it
             SCNTransaction.begin()
             SCNTransaction.animationDuration = 0.5
-            
-            // on completion - unhighlight
+
+            // On completion - unhighlight
             SCNTransaction.completionBlock = {
                 SCNTransaction.begin()
                 SCNTransaction.animationDuration = 0.5
-                
+
                 material.emission.contents = UIColor.black
-                
+
                 SCNTransaction.commit()
             }
-            
+
             material.emission.contents = UIColor.red
-            
+
             SCNTransaction.commit()
         }
     }
-    
-    func setupController(){
+
+    func setupController() {
         let controllerConfig = GCVirtualController.Configuration()
-        
         controllerConfig.elements = [
-        GCInputLeftThumbstick
+            GCInputLeftThumbstick
         ]
-        
+
         let controller = GCVirtualController(configuration: controllerConfig)
         controller.connect()
         virtualController = controller
     }
-    
+
+    // Override this method to perform per-frame game logic
+    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+        guard let virtualController = virtualController else { return }
+
+        // Get left thumbstick values
+        if let thumbstick = virtualController.controller?.extendedGamepad?.leftThumbstick {
+            let xValue = thumbstick.xAxis.value
+            let yValue = thumbstick.yAxis.value
+
+            // Update ship position based on thumbstick values (adjust values as needed)
+            ship.position.x += Float(xValue) * 0.1
+            ship.position.z += Float(yValue) * 0.1
+        }
+    }
+
     override var prefersStatusBarHidden: Bool {
         return true
     }
-    
+
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
         if UIDevice.current.userInterfaceIdiom == .phone {
             return .landscape
@@ -127,5 +143,4 @@ class GameViewController: UIViewController {
             return .all
         }
     }
-
 }
